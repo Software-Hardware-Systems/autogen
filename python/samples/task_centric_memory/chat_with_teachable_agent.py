5326fac2f13fcead67efc8426d9b3803163fd35d
Itrel Monroe <Itrel.Monroe@SoftwareHardwareSystems.com>
2025-03-12 18:28:35 -0500

    task-centric-memory using Azure AI Inference

--- python/samples/task_centric_memory/chat_with_teachable_agent.py
+++ python/samples/task_centric_memory/chat_with_teachable_agent.py
@@ -1,13 +1,32 @@
+import os
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.ui import Console
-from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_ext.models.azure import AzureAIChatCompletionClient
 from autogen_ext.experimental.task_centric_memory import MemoryController
 from autogen_ext.experimental.task_centric_memory.utils import Teachability
-
+from azure.core.credentials import AzureKeyCredential
 
 async def main():
+    # Debugging information
+    endpoint = os.getenv("AZURE_AI_CHAT_ENDPOINT") or ""
+    api_key = os.getenv("AZURE_AI_CHAT_KEY") or ""
+    model = os.getenv("CHAT_MODEL")
+    print(f"Endpoint: {endpoint}")
+    #print(f"API Key: {api_key}")
+    print(f"Model: {model}")
+
     # Create a client
-    client = OpenAIChatCompletionClient(model="gpt-4o-2024-08-06", )
+    client = AzureAIChatCompletionClient(
+        endpoint=endpoint,
+        credential=AzureKeyCredential(api_key),
+        model=model,
+        model_info={
+            "json_output": True,
+            "function_calling": True,
+            "vision": True,
+            "family": "unknown",
+        }
+    )
 
     # Create an instance of Task-Centric Memory, passing minimal parameters for this simple example
     memory_controller = MemoryController(reset=False, client=client)
